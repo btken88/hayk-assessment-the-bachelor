2d4e2650e512533354cca771eddd56253ad136b6
github username <github email address>
2020-05-13 14:52:41 -0600

    Adjusted methods for readability

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,40 +1,5 @@
 require 'pry'
 
-test_data = {
-  "season 30": [
-    {
-      "name":      "Beth Smalls",
-      "age":       "26",
-      "hometown":  "Great Falls, Virginia",
-      "occupation":"Nanny/Freelance Journalist",
-      "status":    "Winner"
-    },
-    {
-      "name":       "Becca Tilley",
-      "age":        "27",
-      "hometown":   "Shreveport, Louisiana",
-      "occupation": "Chiropractic Assistant",
-      "status":     "Eliminated Week 8"
-    }
-  ],
-  "season 29": [
-    {
-      "name":      "Ashley Yeats",
-      "age":       "24",
-      "hometown":  "Denver, Colorado",
-      "occupation": "Dental Assitant",
-      "status":    "Winner"
-    },
-    {
-      "name":       "Sam Grover",
-      "age":        "29",
-      "hometown":   "New York, New York",
-      "occupation": "Entertainer",
-      "status":     "Eliminated Week 6"
-    }
-  ]
-}
-
 def contestant_array(data)
   contestant_array = []
   data.each_pair do |season, contestants|
