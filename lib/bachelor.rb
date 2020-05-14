5542436cd1d2ffd56ecc1e620d3e300051237e72
github username <github email address>
2020-05-14 10:53:29 -0600

    Further cleaning of code

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,34 +1,34 @@
 require 'pry'
 
-def contestant_array(data)
+def contestant_array(seasons)
   contestant_array = []
-  data.each_pair do |season, contestants|
+  seasons.each_pair do |season, contestants|
     contestants.each do |contestant|
-      contestant["season"] = season
+      contestant['season'] = season
       contestant_array << contestant
     end
   end
   contestant_array
 end
 
-def get_first_name_of_season_winner(data, season)
-  winner = data[season].select { |contestant| contestant["status"] == "Winner" } 
-  winner[0]["name"].split.first
+def get_first_name_of_season_winner(seasons, season_label)
+  winner_full_name = seasons[season_label].find { |contestant| contestant['status'] == 'Winner' }
+  winner_full_name['name'].split.first
 end
 
-def get_contestant_name(data, occupation)
-  contestant = contestant_array(data).select { |contestant| contestant["occupation"] == occupation} 
-  contestant[0]["name"]
+def get_contestant_name(seasons, occupation)
+  correct_contestant = contestant_array(seasons).find { |contestant| contestant["occupation"] == occupation} 
+  correct_contestant["name"]
 end
 
-def count_contestants_by_hometown(data, hometown)
-  contestant_array(data).select { |contestant| contestant["hometown"] == hometown }.length
+def count_contestants_by_hometown(seasons, hometown)
+  contestant_array(seasons).select { |contestant| contestant["hometown"] == hometown }.length
 end
 
-def get_occupation(data, hometown)
-  contestant = contestant_array(data).select { |contestant| contestant["hometown"] == hometown}
-  contestant[0]["occupation"]
-  # data.each_pair do |season, contestants|
+def get_occupation(seasons, hometown)
+  correct_contestant = contestant_array(seasons).find { |contestant| contestant["hometown"] == hometown}
+  correct_contestant["occupation"]
+  # seasons.each_pair do |season, contestants|
   #   contestants.each do |contestant|
   #     if contestant["hometown"] == hometown
   #       return contestant["occupation"]
@@ -37,9 +37,9 @@ def get_occupation(data, hometown)
   # end
 end
 
-def get_average_age_for_season(data, season)
-  total_age = data[season].reduce(0) { |age, contestant| age += contestant["age"].to_i }
-  (total_age.to_f/data[season].length).round()
+def get_average_age_for_season(seasons, season)
+  total_age = seasons[season].reduce(0) { |age, contestant| age += contestant["age"].to_i }
+  (total_age.to_f/seasons[season].length).round()
   # count = 0
   # total_age = 0
   # data[season].each do |contestant|
