e386f096e69ede4814eeadf604ef5197140076c8
github username <github email address>
2020-05-14 10:24:02 -0600

    Cleaned up all my functions.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,54 +12,40 @@ def contestant_array(data)
 end
 
 def get_first_name_of_season_winner(data, season)
-  data[season].each do |contestant|
-    if contestant["status"] == "Winner"
-      return contestant["name"].split[0]
-    end
-  end
+  winner = data[season].select { |contestant| contestant["status"] == "Winner" } 
+  winner[0]["name"].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-  contestants = contestant_array(data)
-  contestants.each do |contestant|
-    if contestant["occupation"] == occupation
-      return contestant["name"]
-    end
-  end
+  contestant = contestant_array(data).select { |contestant| contestant["occupation"] == occupation} 
+  contestant[0]["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-  count = 0
-  contestants = contestant_array(data)
-  contestants.each do |contestant|
-    if contestant["hometown"] == hometown
-      count += 1
-    end
-  end
-  count
+  contestant_array(data).select { |contestant| contestant["hometown"] == hometown }.length
 end
 
 def get_occupation(data, hometown)
-  # code here
-  data.each_pair do |season, contestants|
-    contestants.each do |contestant|
-      if contestant["hometown"] == hometown
-        return contestant["occupation"]
-      end
-    end
-  end
+  contestant = contestant_array(data).select { |contestant| contestant["hometown"] == hometown}
+  contestant[0]["occupation"]
+  # data.each_pair do |season, contestants|
+  #   contestants.each do |contestant|
+  #     if contestant["hometown"] == hometown
+  #       return contestant["occupation"]
+  #     end
+  #   end
+  # end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
-  count = 0
-  total_age = 0
-  data[season].each do |contestant|
-    count += 1
-    total_age += contestant["age"].to_i
-  end
-  average = total_age/count.to_f
-  average.round()
+  total_age = data[season].reduce(0) { |age, contestant| age += contestant["age"].to_i }
+  (total_age.to_f/data[season].length).round()
+  # count = 0
+  # total_age = 0
+  # data[season].each do |contestant|
+  #   count += 1
+  #   total_age += contestant["age"].to_i
+  # end
+  # average = total_age/count.to_f
+  # average.round()
 end
