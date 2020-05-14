925662f491de83935a4ed914724ab80c88fa95cb
github username <github email address>
2020-05-14 11:22:07 -0600

    Final cleanup.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,32 +1,36 @@
 require 'pry'
 
-def contestant_array(seasons)
-  contestant_array = []
-  seasons.each_pair do |season, contestants|
-    contestants.each do |contestant|
+def get_all_contestants(seasons)
+  seasons.reduce([]) do |all_contestants, (season, contestants)|
+    contestants_with_label = contestants.map do |contestant|
       contestant['season'] = season
-      contestant_array << contestant
+      contestant
     end
+    all_contestants.concat contestants_with_label
   end
-  contestant_array
 end
 
 def get_first_name_of_season_winner(seasons, season_label)
-  winner_full_name = seasons[season_label].find { |contestant| contestant['status'] == 'Winner' }
+  winner_full_name = seasons[season_label]
+    .find { |contestant| contestant['status'] == 'Winner' }
   winner_full_name['name'].split.first
 end
 
 def get_contestant_name(seasons, occupation)
-  correct_contestant = contestant_array(seasons).find { |contestant| contestant["occupation"] == occupation} 
+  correct_contestant = get_all_contestants(seasons)
+    .find { |contestant| contestant["occupation"] == occupation} 
   correct_contestant["name"]
 end
 
 def count_contestants_by_hometown(seasons, hometown)
-  contestant_array(seasons).select { |contestant| contestant["hometown"] == hometown }.length
+  get_all_contestants(seasons)
+    .select { |contestant| contestant["hometown"] == hometown }
+    .length
 end
 
 def get_occupation(seasons, hometown)
-  correct_contestant = contestant_array(seasons).find { |contestant| contestant["hometown"] == hometown}
+  correct_contestant = get_all_contestants(seasons)
+    .find { |contestant| contestant["hometown"] == hometown}
   correct_contestant["occupation"]
   # seasons.each_pair do |season, contestants|
   #   contestants.each do |contestant|
@@ -38,7 +42,8 @@ def get_occupation(seasons, hometown)
 end
 
 def get_average_age_for_season(seasons, season_label)
-  total_age = seasons[season_label].reduce(0) { |age, contestant| age += contestant["age"].to_i }
+  total_age = seasons[season_label]
+    .reduce(0) { |age, contestant| age += contestant["age"].to_i }
   (total_age.to_f/seasons[season_label].length).round()
   # count = 0
   # total_age = 0
